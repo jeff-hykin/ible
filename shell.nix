22652bddaa190de145ee1f19bb0e86d846343ab1
jeff-hykin <jeff.hykin@gmail.com>
2020-08-09 20:52:28 -0500

    - add nix!!

---
+++ shell.nix
@@ -0,0 +1,19 @@
+# Lets setup some definitions
+let
+    # the niv tool should pin your current thing inside ./nix/sources
+    # here we go and get that pinned version so we can pull packages out of it
+    sources = import ./nix/sources.nix;
+    normalPackages = import sources.nixpkgs {};
+
+# using those definitions
+in
+    # create a shell
+    normalPackages.mkShell {
+        
+        # inside that shell, make sure to use these packages
+        buildInputs = [
+            normalPackages.nodejs-14_x
+            normalPackages.bashInteractive
+        ];
+        
+    }
