efc5eb19980807e5e7213a99ee51dcf2eff057c2
jeff-hykin <jeff.hykin@gmail.com>
2020-09-01 11:50:01 -0500

    improve moments logic

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -27,9 +27,9 @@
                     row.level(v-for="(eachLevel, index) in organizedSegments" align-h="space-between" position="relative")
                         row.segment(
                             v-for="(eachSegment, index) in eachLevel"
+                            :left="eachSegment.leftPercent"
+                            :width="eachSegment.widthPercent"
                             @click="jumpSegment(eachSegment.index)"
-                            :left="`${(eachSegment.effectiveStart/duration)*100}%`"
-                            :width="`${(eachSegment.effectiveEnd - eachSegment.effectiveStart)*100/duration}%`"
                         )
                             ui-tooltip(position="left" animation="fade")
                                 | label: {{ eachSegment.label }}
@@ -53,10 +53,18 @@ export let videoEvents = new EventEmitter()
 import { segmentEvents } from "./Segments"
 import index from 'vue-youtube-embed'
 
-let { dynamicSort } = require("good-js")
+let { dynamicSort, logBlock } = require("good-js")
 let windowListenerMixin = require("../mixins/window-listeners")
 
 
+
+let video = {
+    IS_LOADING: -1,
+    IS_PLAYING: 1,
+    IS_PAUSED: 2,
+    HASNT_EVEN_INITILIZED: null,
+    HASNT_STARTED_STATE: 5,
+}
 
 export default {
     props: [ 'segments' ],
@@ -67,13 +75,16 @@ export default {
     data: ()=>({
         player: null,
         organizedSegments: [],
+        whichVideo: null,
         whichSegment: 0,
         duration: null,
         videoInitilized: false,
+        scheduledToggle: {},
         windowListeners$: {
             keydown(eventObj) {
+                console.debug(`eventObj.key is:`,eventObj.key)
                 // 
-                // arrow key controls
+                // key controls
                 // 
                 switch (eventObj.key) {
                     case "ArrowRight":
@@ -81,6 +92,9 @@ export default {
                         break
                     case "ArrowLeft":
                         this.decrementIndex()
+                        break
+                    case " ":
+                        this.player.pauseVideo()
                         break
                     default:
                         // we dont care about other keys
@@ -100,13 +114,32 @@ export default {
         segments(value, oldValue) {
             // reset the segment count
             this.whichSegment = 0
-            this.videoInitilized = false
+            this.checkIfVideoChanged()
             this.seekToSegmentStart()
             this.organizeSegments()
         },
         whichSegment(value) {
             videoEvents.emit("whichSegment:update", { whichSegment: this.whichSegment })
+            this.checkIfVideoChanged()
             this.seekToSegmentStart()
+            this.organizeSegments()
+        },
+        // when the video changes
+        whichVideo(value) {
+            logBlock({name: "whichVideo"}, ()=>{
+                console.debug(`this.whichVideo is:`, this.whichVideo)
+                // it hasn't been initilized
+                this.videoInitilized = false
+                // manually wipe the info (otherwise old video info will still be there)
+                if (this.player) {
+                    this.player.playerInfo.duration = null
+                    console.debug(`this.player.getDuration() is:`,this.player.getDuration())
+                }
+                // load / init the video
+                this.seekToSegmentStart()
+                // the duration changed so the segments need to be recalculated
+                this.organizeSegments()
+            })
         }
     },
     computed: {
@@ -115,57 +148,74 @@ export default {
         }
     },
     methods: {
+        checkIfVideoChanged() {
+            if (this.segment.video_id != this.whichVideo) {
+                this.whichVideo = this.segment.video_id
+            }
+        },
         organizeSegments() {
-            let whichVideo = this.segment.video_id
-            let videoSegments = []
-            // 2 percent of the width of the video
-            let minWidth = this.duration / 50
-            if (whichVideo) {
-                videoSegments = this.segments.map((each, index)=>{
-                    let effectiveStart = each.start
-                    let effectiveEnd = each.end
-                    let segmentDuration = each.end - each.start
-                    // if segment is too small artificially make it bigger
-                    if (segmentDuration < minWidth) {
-                        let additionalAmount = (minWidth - segmentDuration)/2
-                        // sometimes this will result in a negative amount, but thats okay
-                        // the UI can handle it, the user just needs to be able to see it
-                        effectiveStart -= additionalAmount
-                        effectiveEnd += additionalAmount
-                    }
-                    return {
-                        ...each,
-                        index,
-                        effectiveStart,
-                        effectiveEnd,
-                    }
-                }).filter(each=>each.video_id == whichVideo)
-            }
-            let levels = []
-            for (let eachSegment of videoSegments.sort(dynamicSort("effectiveStart"))) {
-                // find the smallest viable level
-                let level = 0
-                while (levels[level] != undefined && eachSegment.effectiveStart <= levels[level][ levels[level].length-1 ].effectiveEnd) {
-                    ++level
+            logBlock({name: "organizeSegments"}, ()=>{
+                // wait until player is initilized
+                if (!this.videoInitilized) {
+                    return setTimeout(() => this.organizeSegments(), 100)
                 }
-                // create level if it didn't exist
-                if (levels[level] == undefined) {
-                    levels[level] = [ eachSegment ]
-                // otherwise add it to the end of the level
-                } else {
-                    levels[level].push(eachSegment)
+                let videoSegments = []
+                // 2 percent of the width of the video
+                if (this.whichVideo) {
+                    let duration = this.player.getDuration()
+                    console.debug(`duration is:`,duration)
+                    let minWidth = duration / 50
+                    videoSegments = this.segments.map((each, index)=>{
+                        let effectiveStart = each.start
+                        let effectiveEnd = each.end
+                        let segmentDuration = each.end - each.start
+                        // if segment is too small artificially make it bigger
+                        if (segmentDuration < minWidth) {
+                            let additionalAmount = (minWidth - segmentDuration)/2
+                            // sometimes this will result in a negative amount, but thats okay
+                            // the UI can handle it, the user just needs to be able to see it
+                            effectiveStart -= additionalAmount
+                            effectiveEnd += additionalAmount
+                        }
+                        return {
+                            ...each,
+                            index,
+                            effectiveEnd,
+                            effectiveStart,
+                            // how wide the element should be
+                            widthPercent: `${(effectiveEnd - effectiveStart)*100/duration}%`,
+                            // how close to the left the element should be
+                            leftPercent: `${(effectiveStart/duration)*100}%`,
+                        }
+                    }).filter(each=>each.video_id == this.whichVideo)
                 }
-            }
-            this.organizedSegments = levels
+                let levels = []
+                for (let eachSegment of videoSegments.sort(dynamicSort("effectiveStart"))) {
+                    // find the smallest viable level
+                    let level = 0
+                    while (levels[level] != undefined && eachSegment.effectiveStart <= levels[level][ levels[level].length-1 ].effectiveEnd) {
+                        ++level
+                    }
+                    // create level if it didn't exist
+                    if (levels[level] == undefined) {
+                        levels[level] = [ eachSegment ]
+                    // otherwise add it to the end of the level
+                    } else {
+                        levels[level].push(eachSegment)
+                    }
+                }
+                this.organizedSegments = levels
+            })
         },
         ready(event) {
+            console.log(`video is ready`)
             this.player = event.target
-            this.duration = this.player.getDuration()
-            this.organizeSegments()
             window.player = this.player
+            this.organizeSegments()
         },
         playing() {
-            console.log(`playing`)
+            console.log(`video is playing`)
+            this.player.playerInfo.playerState = video.IS_PLAYING
         },
         waitThenPause(seekBackToStart=null) {
             // if already playing
@@ -205,23 +255,83 @@ export default {
         seekToSegmentStart() {
             // if the player doesn't exist, reschedule the seek action
             if (!this.player) {
-                return setTimeout(() => {
-                    this.seekToSegmentStart()
-                }, 0)
+                return setTimeout(()=>this.seekToSegmentStart(), 0)
             }
             this.player.seekTo(this.segment.start)
             
             // in this state the player (after seeking) will start playing
             // which isn't the best UX, so pause it immediately
-            const VIDEO_HASNT_STARTED_STATE = 5
-            const VIDEO_PAUSED_STATE = 2
-            let state = this.player.getPlayerState()
-            if (state == VIDEO_HASNT_STARTED_STATE || (state == VIDEO_PAUSED_STATE && !this.videoInitilized)) {
+            let state = this.player.playerInfo.playerState
+            if (state == video.HASNT_STARTED_STATE || (state == video.IS_PAUSED && !this.videoInitilized)) {
                 let seekBackToStart = true
                 // give it enough time to load the frame (otherwise it loads infinitely)
                 this.player.playVideo()
                 // tell the pause exactly where to jump back to after pausing
                 this.waitThenPause(seekBackToStart)
+            }
+        },
+        pauseVideo() {
+            // TODO: check me
+            
+            // cancel scheduled play
+            if (this.scheduledToggle.type == "play") {
+                clearTimeout(this.scheduledToggle.id)
+            }
+            // reset the schedule
+            this.scheduledToggle = {}
+            
+            if (!this.player) {
+                return
+            }
+            
+            this.player.pauseVideo()
+            // if video is playing
+            if (this.player.playerInfo.playerState == video.IS_PLAYING) {
+                // schedule another pause for good measure
+                this.scheduledToggle.type = "pause"
+                this.scheduledToggle.id = setTimeout(()=>this.pauseVideo(), 50)
+            }
+        },
+        playVideo() {
+            // TODO: check me
+            
+            // cancel scheduled pause
+            if (this.scheduledToggle.type == "pause") {
+                clearTimeout(this.scheduledToggle.id)
+            }
+            // reset the schedule
+            this.scheduledToggle = {}
+            
+            if (!this.player) {
+                return
+            }
+            // try to play
+            this.player.playVideo()
+            
+            // if video isn't playing
+            if (this.player.playerInfo.playerState != video.IS_PLAYING) {
+                // schedule another play for good measure
+                this.scheduledToggle.type = "play"
+                this.scheduledToggle.id = setTimeout(()=>this.playVideo(), 50)
+            }
+        },
+        videoIsPaused() {
+            // TODO: check me
+            
+            if (this.scheduledToggle.type == "play") {
+                return false
+            }
+            if (this.player == null || this.scheduledToggle.type == "pause") {
+                return true
+            }
+            switch (this.player.playerInfo.playerState) {
+                case video.IS_PLAYING:
+                    return false
+                case video.HASNT_EVEN_INITILIZED:
+                case video.HASNT_STARTED_STATE:
+                case video.IS_LOADING: // this might not always be true, but I'm not sure
+                case video.IS_PAUSED:
+                    return true
             }
         }
     }
