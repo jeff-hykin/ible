d05a249977ef3184865d353275de04faf0d8a676
jeff-hykin <jeff.hykin@gmail.com>
2020-11-12 23:22:28 +0000

    new version built

--- docs/Root.875ac4b3.js
+++ docs/Root.875ac4b3.js
@@ -32093,12 +32093,13 @@ let Vue = require("vue").default;
 let ezRpc = require("ez-rpc-frontend"); // const databaseUrl = "http://192.168.86.198:3000"
 // const databaseUrl = "http://localhost:3000"
 // const databaseUrl = "http://paradise.cs.tamu.edu:3000"
-// const databaseUrl = "http://128.194.4.15:3000" // csce-jiang1.engr.tamu.edu:3000
 // const databaseUrl = "http://192.168.192.57:3000"
-// const databaseUrl = "http://192.168.192.137:3000" // my desktop db 
+// const ezRpcUrl = "http://192.168.192.137:54321" // my desktop db 
+// const ezRpcUrl = "http://128.194.4.15:3000" // csce-jiang1.engr.tamu.edu:3000
 
 
-const ezRpcUrl = "http://192.168.192.137:54321";
+const ezRpcUrl = "http://128.194.4.15:6283"; // csce-jiang1.engr.tamu.edu:6283
+
 const key = "4a75cfe3cdc1164b67aae6b413c9714280d2f102";
 window.backend = ezRpc.buildInterfaceFor(ezRpcUrl);
 module.exports = {
