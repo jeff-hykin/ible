40d7394500419291d12c7197bb224b371763ca9b
Jeff Hykin <jeff.hykin@gmail.com>
2020-11-12 23:46:45 +0000

    -

--- shell.nix
+++ shell.nix
@@ -83,6 +83,11 @@ let
     
     # TODO: add support for the info.json to have OS-specific packages (if statement inside package inclusion)
     packagesForMacOnly = [] ++ definitions.mainPackages.lib.optionals (definitions.mainPackages.stdenv.isDarwin) [
+        # python and venv
+        definitions.mainPackages.python37
+        definitions.mainPackages.python37Packages.setuptools
+        definitions.mainPackages.python37Packages.pip
+        definitions.mainPackages.python37Packages.virtualenv
     ];
 # using those definitions
 in
