275cdb4e7a819714b4e2dff7ac04a21178b4a6f2
Jeff Hykin <jeff.hykin@gmail.com>
2024-06-24 19:36:17 -0500

    -

--- src/tooling/video_tooling.js
+++ src/tooling/video_tooling.js
@@ -8,7 +8,6 @@ export const isLocalVideo = (videoId) => videoId && !(
         (videoId.match(/.*youtu\.be\//) && videoId.match(/.*youtu.be\/(.{11})/))
     )
     // toString(videoId).replace(/\\videos\\/g, "/videos/").startsWith(localVideoPrefix)
-export const getLocalVideoName = (videoPath) => videoPath
 export const minSizeOfUnixTimestamp = 10
 export const currentFixedSizeOfYouTubeVideoId = 11
 export const minSizeOfLocalVideoId = localVideoPrefix.length
