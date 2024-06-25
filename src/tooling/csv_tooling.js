0a1f93329ccf74eef3b01fa5bc43eb621cf64da0
Jeff Hykin <jeff.hykin@gmail.com>
2024-06-25 18:49:54 -0500

    invent .yaml.csv

--- src/tooling/csv_tooling.js
+++ src/tooling/csv_tooling.js
@@ -1,7 +1,10 @@
 import * as csv from "csv/dist/esm/index.js"
+import * as yaml from "yaml"
 
+window.yaml = yaml
 window.csv = csv
-export const rowify = (data, { defaultHeaders=[] }={}) => {
+
+export const rowify = (data, { defaultHeaders=[], delimiter="\t" }={}) => {
     let rows = data
     if (!(data[0] instanceof Array)) {
         const headers = [...defaultHeaders]
@@ -23,17 +26,45 @@ export const rowify = (data, { defaultHeaders=[] }={}) => {
         // pad out rows as needed
         for (const eachRow of rows) {
             while (eachRow.length < headers.length) {
-                eachRow.push("")
+                eachRow.push(null)
             }
         }
     }
     return rows
 }
 
-export function convertToCsv(data, { defaultHeaders=[] }={}) {
+export function convertToCsv(data, { defaultHeaders=[], delimiter="\t" }={}) {
     let rows = rowify(data, { defaultHeaders })
+    // this is a way of getting around that CSV treats null and undefined as empty strings
+    for (const eachRow of rows) {
+        let index = -1
+        for (const each of eachRow) {
+            index++
+            if (each === "") {
+                eachRow[index] =  '""'
+                continue
+            }
+            if (each == null) {
+                eachRow[index] =  ""
+                continue
+            }
+            if (typeof each != "string") {
+                let newString = yaml.stringify(each)
+                if (newString[newString.length-1] == "\n") {
+                    newString = newString.slice(0,-1)
+                }
+                eachRow[index] = newString
+                continue
+            }
+            eachRow[index] = yaml.stringify(each)
+            // don't have a trailing newline if its not needed (it IS needed for block-strings AFAIK)
+            if (eachRow[index].length-1 == each?.length && eachRow[index].slice(-1)[0] === "\n") {
+                eachRow[index] = each
+            }
+        }
+    }
     return new Promise((resolve, reject) => {
-        csv.stringify(rows, (err, output) => {
+        csv.stringify(rows, { delimiter }, (err, output) => {
             if (err) {
                 return reject(err)
             }
@@ -42,13 +73,29 @@ export function convertToCsv(data, { defaultHeaders=[] }={}) {
     })
 }
 
-export function parseCsv(csvString) {
-    return new Promise((resolve, reject) => {
-        csv.parse(csvString, (err, output) => {
+export async function parseCsv(csvString) {
+    const rows = new Promise((resolve, reject) => {
+        csv.parse(csvString, { delimiter }, (err, output) => {
             if (err) {
                 return reject(err)
             }
             resolve(output)
         })
     })
+    for (const eachRow of rows) {
+        let index = -1
+        for (const each of eachRow) {
+            index++
+            try {
+                if (each.match(/^\d{1,4}-\d{1,2}-\d{1,2}T\d{1,2}:\d{1,2}:\d{1,2}(\.\d+)?(Z|[+-]\d{1,2}(:\d{1,2})?)$/)) {
+                    eachRow[index] = new Date(each)
+                } else {
+                    eachRow[index] = yaml.parse(each)
+                }
+            } catch (error) {
+                eachRow[index] = each
+            }
+        }
+    }
+    return rows
 }
